ee26da2cd86b852c2b5181b0edd319610c4f6126
Maximiliano Martinez <maximilianomartinez15@outlook.com>
2019-01-23 23:12:27 -0500

    ...

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-fetch("http://www.omdbapi.com/?t=aquaman&apikey=bfcb574d")
+// this is for finding a movie by its title
+// let movieTitle = document.getElementById('movie-search').value
+fetch(`http://www.omdbapi.com/?t=${movieTitle}&apikey=bfcb574d`)
     .then(response => {
-        response.json()
-    })
-    .then(response => {
-        console.log(response)
+        console.log(response.json())
     })
     .catch(error => console.log(error))
