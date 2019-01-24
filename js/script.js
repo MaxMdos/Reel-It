45243026f8e1fda9cbc511e646eef520625ecf4f
Maximiliano Martinez <maximilianomartinez15@outlook.com>
2019-01-23 23:44:33 -0500

    tried forms

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 // this is for finding a movie by its title
-// let movieTitle = document.getElementById('movie-search').value
-fetch(`http://www.omdbapi.com/?t=${movieTitle}&apikey=bfcb574d`)
+
+let titleSearch = document.getElementById('movie-title'); 
+titleSearch.onsubmit = displayTitle(); 
+
+function displayTitle(){
+    let movieTitle = document.getElementById('movie-search').value;
+    fetch(`http://www.omdbapi.com/?t=${movieTitle}&apikey=bfcb574d`)
     .then(response => {
         console.log(response.json())
     })
     .catch(error => console.log(error))
+}
--- scripts/script.js
+++
@@ -1,5 +0,0 @@
-fetch("http://www.omdbapi.com/?apikey=bfcb574d")
-    .then(response => {
-        response.json()
-        console.log(response)
-    })
