6f723ac9437f361f55cf47844958ce599bf4f91c
Maximiliano Martinez <maximilianomartinez15@outlook.com>
2019-01-28 08:43:02 -0500

    added html

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
 // this is for finding a movie by its title
+let button = document.getElementById('butt'); 
+let root = document.getElementById('root'); 
+let result = document.getElementById('movie-res');
+// result = ""; 
 
-let titleSearch = document.getElementById('movie-title'); 
-titleSearch.onsubmit = displayTitle(); 
-
-function displayTitle(){
-    let movieTitle = document.getElementById('movie-search').value;
+button.onclick = () => {
+    let movieTitle = document.getElementById('title-search').value
     fetch(`http://www.omdbapi.com/?t=${movieTitle}&apikey=bfcb574d`)
+    // returns promise - just the promise...
+    .then(response => response.json())
+    // returns the actual json thingy...
     .then(response => {
-        console.log(response.json())
+        console.log(response)
+        apTitle(response)
     })
     .catch(error => console.log(error))
+}
+
+function apTitle(movie){
+   result.innerHTML = `
+   <img src='${movie.Poster}'>
+   <h1>${movie.Title}</h1>
+   <p>${movie.Director}</p>
+   <p>${movie.Released}</p>
+   `
 }
